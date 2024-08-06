9fc50390fa771480932e9ea9e4c1f6255b66e762
Magnitrude <100609084+Magnitrude@users.noreply.github.com>
2024-08-05 22:20:41 -0400

    small resources fix

--- qbx_tackle/server.lua
+++ qbx_tackle/server.lua
@@ -7,4 +7,5 @@ RegisterNetEvent('tackle:server:TacklePlayer', function(target)
     if #(srcCoords - targetCoords) > 2.0 then return end
 
     TriggerClientEvent('tackle:client:GetTackled', target)
+
 end)
